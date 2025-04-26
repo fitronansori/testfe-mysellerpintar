a82f6ad42385581cdab273f20aad4dd8f9cb7b2e
fitronansori <fitronansori7@gmail.com>
2025-04-26 21:10:52 +0800

    feat: add CategoryPage component with DashboardHeader

---
+++ src/app/(dashboard)/dashboard/category/page.tsx
@@ -0,0 +1,11 @@
+import DasboardHeader from "@/components/layouts/AppSidebar/DashboardHeader";
+
+const CategoryPage = () => {
+  return (
+    <section>
+      <DasboardHeader pageName="Category" />
+      CategoryPage
+    </section>
+  );
+};
+export default CategoryPage;
